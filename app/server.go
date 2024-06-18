5b127c005fb1d6e7cf7b4c95b08e664e85ca0175
danradchuk <radchuk@fastmail.com>
2024-06-18 03:33:31 +0200

    Extract URL path

--- app/server.go
+++ app/server.go
@@ -6,6 +6,10 @@ import (
 	"os"
 )
 
+const empty200 = "HTTP/1.1 200 OK\r\n\r\n"
+const empty400 = "HTTP/1.1 400 BAD REQUEST\r\n\r\n"
+const empty404 = "HTTP/1.1 404 Not Found\r\n\r\n"
+
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
@@ -22,11 +26,44 @@ func main() {
 		os.Exit(1)
 	}
 
-	n, err := conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+	var b []byte
+	nRead, err := conn.Read(b)
 	if err != nil {
-		fmt.Println("Error writing the response: ", err.Error())
+		fmt.Println("Error reading the request: ", err.Error())
 		os.Exit(1)
 	}
 
-	fmt.Printf("the size of the message is %d", n)
+	if nRead == 0 {
+		conn.Write([]byte(empty400))
+		os.Exit(1)
+	}
+
+	// extract URL
+	var i int
+	var start int
+	var end int
+	for i != nRead {
+		if b[i] == ' ' {
+			if start == 0 {
+				start = i
+			} else {
+				end = i
+				break
+			}
+		}
+		i++
+	}
+
+	url := b[start:end]
+	if len(url) == 1 {
+		conn.Write([]byte(empty200))
+	} else {
+		conn.Write([]byte(empty404))
+	}
+
+	// nWrite, err := conn.Write([]byte(empty200))
+	// if err != nil {
+	// 	fmt.Println("Error writing the response: ", err.Error())
+	// 	os.Exit(1)
+	// }
 }
